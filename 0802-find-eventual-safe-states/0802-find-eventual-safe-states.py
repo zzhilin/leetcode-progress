e522a747189fb7c49234119f9d02520ea1240cbd
zzhilin <59615877+zzhilin@users.noreply.github.com>
2023-11-04 23:26:40 -0400

    Time: 654 ms (12.32%), Space: 23.9 MB (56.51%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -1,26 +1,44 @@
 class Solution:
-    def is_safe(self, node, graph, visited,safe_nodes):
-        if visited[node] == 1:
-            return True
-        if visited[node] == -1:
-            return False
-        
-        visited[node] = -1
-        # print(node,visited)
-        # check neighbor safe or not
-        for nei in graph[node]:
-            if not self.is_safe(nei, graph, visited,safe_nodes):
-                return False
-        visited[node] = 1
-        return True
-    
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        # failed 60
         
-        # dfs
-        visited = [0]*len(graph)
-        safe_nodes = [] # res
+        # get indegree
+        indegree = {node: 0 for node in range(len(graph))}
         for i in range(len(graph)):
-            if self.is_safe(i, graph, visited,safe_nodes):
-                safe_nodes.append(i)
-        return safe_nodes
+            for nei in graph[i]:
+                indegree[i] += 1
+                
+        q = deque()
+        # get adj list
+        reverse_g = defaultdict(list)
+        for i in range(len(graph)):
+            for node in graph[i]:
+                reverse_g[node].append(i)
+        
+        
+        
+        # q for bfs, add all nodes with indegree = 0
+        for i in range(len(graph)):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        # track safe nodes
+        safe = [False] * len(graph)
+        
+        # pop, mark as safe, 
+        while q:
+            node = q.popleft()
+            safe[node] = True
+            for nei in reverse_g[node]:
+                # - indeg nei by 1
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        res = []
+        for i in range(len(graph)):
+            if safe[i]:
+                res.append(i)
+                
+        return res
+        
+        
+        
