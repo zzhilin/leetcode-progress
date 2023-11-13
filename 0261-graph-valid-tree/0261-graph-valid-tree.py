b04276c5306e9b1087e0611a6ce6182188ab3f53
zzhilin <59615877+zzhilin@users.noreply.github.com>
2023-11-12 22:47:30 -0500

    Time: 100 ms (30.06%), Space: 17.7 MB (68.39%) - LeetHub

---
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -0,0 +1,29 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        """
+        check no cycles and is connected
+        dfs or bfs - bfs
+        time: V*E
+        space: (V)
+        """
+        visited = set()
+        q = deque([0]) #cnt = 0
+        visited_nodes = 0
+        graph = defaultdict(list)
+        for x,y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+            
+        while q:
+            node = q.popleft()
+            visited_nodes += 1
+            if node in visited:
+                return False
+            visited.add(node)
+            for nei in graph[node]:
+                if nei not in visited:
+                    q.append(nei)
+        if visited_nodes != n:
+            return False
+        return True
+        
