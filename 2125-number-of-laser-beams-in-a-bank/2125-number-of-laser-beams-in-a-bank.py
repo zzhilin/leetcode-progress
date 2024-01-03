7bae040bbfb6d874f55bc47e1806251a225b6134
zzhilin <59615877+zzhilin@users.noreply.github.com>
2024-01-02 21:46:19 -0500

    Time: 79 ms (98.89%), Space: 19.4 MB (17.25%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,17 +1,16 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
         res = 0
-        pos = defaultdict(int) #row: pos
-        for i in range(len(bank)):
-            for j in range(len(bank[0])):
-                if bank[i][j] == "1":
-                    pos[i]+=1
-        # print(pos)
-        
-        rows = list(pos.keys())
-        if len(rows) <= 1:
-            return res
-        for i in range(len(rows)-1):
-            res += pos[rows[i]] * pos[rows[i+1]]
-        
+        prev = 0
+        for i, beams in enumerate(bank):
+            cnt = beams.count('1')
+            if cnt == 0:
+                continue
+            if not prev:
+                prev = cnt
+                continue
+                
+            else:
+                res = res + (prev * cnt)
+                prev = cnt
         return res
