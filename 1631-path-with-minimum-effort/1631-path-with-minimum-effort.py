73a56373b9edf06ab3b8a9f0b7670a45633c235e
zzhilin <59615877+zzhilin@users.noreply.github.com>
2024-01-13 16:59:03 -0500

    Time: 488 ms (94.22%), Space: 18.7 MB (66.35%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,44 +1,26 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        '''
-        input: 2d arr, each cell is height
-        start at 0,0
-        end at m-1,n-1
-        effort is largest diff in height between two cell next to each other
-        return min effort (find max in one path, min in all paths)
-        
-        constraints
-        - height is always positive?
-        
-        test
-        - heights= [[1]] return 0
-        - single row [[1,2,4]] return 2 only one path
-        - same height [[1,1,1],[1,1,1]] return 0
-        
-        compare each cell height
-        
-        '''
+        """
+        modified dijkstra to find lower weight path
+        use heap that takes diff, diff matrix to track effort
+        """
         m,n = len(heights),len(heights[0])
-        q = [(0,0,0)]
-        q.append((0,0,0))
+        pq = [(0,0,0)]
         effort = [[float("inf") for _ in range(n)] for _ in range(m)]
-        effort[0][0]=0
+        effort[0][0] = 0
         visited = [[0 for _ in range(n)] for _ in range(m)]
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
         
-        while q:
-            
-            diff,i,j = heapq.heappop(q)
-
+        while pq:
+            diff,i,j = heappop(pq)
             visited[i][j] = 1
-            for dx,dy in directions:
+            for dx,dy in [(0,1),(1,0),(0,-1),(-1,0)]:
                 r,c = i+dx,j+dy
+                # validation
                 if 0<=r<m and 0<=c<n and not visited[r][c]:
                     curr_diff = abs(heights[r][c] - heights[i][j])
                     max_diff = max(curr_diff, effort[i][j])
-                    if effort[r][c] > max_diff:
+                    if max_diff < effort[r][c]:
                         effort[r][c] = max_diff
-                        heapq.heappush(q, (max_diff,r,c))
+                        heapq.heappush(pq, (max_diff,r,c))
         return effort[-1][-1]
                     
-        return 0
