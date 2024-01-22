42474e0b1f4d7a8dfffad31cb2633cdcb0eb827c
zzhilin <59615877+zzhilin@users.noreply.github.com>
2024-01-22 17:32:25 -0500

    Time: 36 ms (67.47%), Space: 16.6 MB (86.51%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,25 +1,28 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        # find number of ways to decode
-        # dp
-        # topdown
+        # dp[i]: end at curr digit, how many ways to decode / how many combinations
+        # if s[i] == 0, return 0 (cannot decode)
+        # basically if cannot decode, dp[i] = dp[i-1] * 1 if s[i] != 0
+        # if curr and prev can decode, dp[i] = dp[i] + dp[i-2]*1
         
-        # 121 127 012 102
-        # start with 0-> terminate
-        # find number of ways at curr index
-        # curr number is more than 26 -> terminate
-        
+        dp = {len(s): 1}
         def dfs(i):
-            if i in memo:
-                return memo[i]
+            
+            # good: already cached curr s[i] or we are at the end
+            if i in dp:
+                return dp[i]
+            # bad base case: s[i] is 0
             if s[i] == "0":
                 return 0
             
+            # here solve our sub problem: s[i] is between 1 to 9, and we take only single digit
             res = dfs(i+1)
+            # case we can take 2: next is in bound and valid (< 27)
             if (i+1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456")):
                 res += dfs(i+2)
-            memo[i] = res
+                
+            dp[i] = res
             return res
         
-        memo = {len(s): 1} # default num of ways at n
+        # how many ways we can decode s start at 0
         return dfs(0)
