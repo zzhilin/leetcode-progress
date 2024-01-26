1b8a0ae7fca1a4ffcc0d319b349c69016ce516c7
zzhilin <59615877+zzhilin@users.noreply.github.com>
2024-01-26 10:48:50 -0500

    Time: 244 ms (77.95%), Space: 18.9 MB (82.71%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,23 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i,j,visited,grid):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]):
-                return False
-            if grid[i][j] != '1':
-                return False
-            if (i,j) in visited:
-                return False
-            visited.add((i,j))
-            dfs(i+1,j,visited,grid)
-            dfs(i-1,j,visited,grid)
-            dfs(i,j+1,visited,grid)
-            dfs(i,j-1,visited,grid)
-            return True
-            
-        visited = set()
-        cnt = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if dfs(i,j,visited,grid):
-                    cnt += 1
-        return cnt
+        # 1 is root node, trigger dfs
+        # number of root node is number of islands
+        # mark visited
+        m,n = len(grid),len(grid[0])
+        num_islands = 0
+        
+        def is_valid(i,j):
+            return 0 <= i < m and 0 <= j < n and grid[i][j] != '2' and grid[i][j] != '0'
+        
+        def dfs(grid,i,j):
+            if not is_valid(i,j):
+                return
+            grid[i][j] = '2'
+            dfs(grid,i+1,j)
+            dfs(grid,i-1,j)
+            dfs(grid,i,j+1)
+            dfs(grid,i,j-1)
+        
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    num_islands += 1
+                    dfs(grid,i,j)
+                    
+        return num_islands
+                    
+        
