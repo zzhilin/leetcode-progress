d38852d638c932e63d00eba9c8ccd9cb3da93380
zzhilin <59615877+zzhilin@users.noreply.github.com>
2024-03-09 11:00:43 -0500

    Time: 795 ms (90.89%), Space: 60.3 MB (63.80%) - LeetHub

--- 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.py
+++ 2049-count-nodes-with-the-highest-score/2049-count-nodes-with-the-highest-score.py
@@ -1,22 +1,24 @@
 class Solution:
     def countHighestScoreNodes(self, parents: List[int]) -> int:
-        # cnt number of nodes
-        high_cnt = 0
-        freq = defaultdict(int)
+        #init graph
+        #use defaultdict to keep track of all scores for the number of nodes with highest scores
         graph = collections.defaultdict(list)
+        scores = collections.defaultdict(int)
         for i in range(len(parents)):
-            if parents[i] >= 0:
-                graph[parents[i]].append(i)
+            graph[parents[i]].append(i)
+        
+        # depth firs search for all nodes' sizes and scores
         def dfs(node):
-
             product, size = 1, 1
             for nei in graph[node]:
-                cnt = dfs(nei)
-                product *= cnt
-                size += cnt
-                
-            product *= len(parents)-size or 1
-            freq[product] += 1
+                nei_size = dfs(nei)
+                # multiply product with child subtree sizes
+                product *= nei_size
+                size += nei_size
+            # multiply product with other subtrees not connected to node
+            product *= len(parents) - size or 1
+            scores[product] += 1
             return size
+        
         dfs(0)
-        return freq[max(freq)]
+        return scores[max(scores)]
